89d19de39ad6366328a89177886247f01ebd6f1d
Empty Chan <chen19941018@live.com>
2017-09-16 12:50:50 +0800

    add some comment

--- server_get_download_page.js
+++ server_get_download_page.js
@@ -3,6 +3,10 @@ var port = 8089;
 var server = require('webserver').create();
  
 server.listen(8089, function (request, response) {
+	//传入的参数有待更改，目前为
+	//{"Key":"https://acg12.com/download/#60e21d8417ab60fbfJfcqnT1BC8Qd20PehAIKv3J4ZO%2FJCo0htE9hP5IFZU", 
+	//"Value":"Tappable-inactive btn btn-success btn-block"}的json字符窜
+	//第一个参数为经过第一次请求所返回的下载页面，第二个为下载按钮的Dom
 	var data = JSON.parse(request.postRaw);
 	var url = data.Key.toString();
 	console.log(url);
@@ -26,6 +30,7 @@ server.listen(8089, function (request, response) {
 		'Connection': 'Keep-Alive',
 		'Keep-Alive': 'timeout=40, max=100'
 	};
+	//根据Phantomjs的官网，这个回调在打开新标签页会触发
 	page.onPageCreated = function(newPage) {
 		//console.log('A new child page was created! Its requested URL is not yet available, though.');
 		page.onInitialized = function() {
@@ -36,15 +41,17 @@ server.listen(8089, function (request, response) {
 			"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"
 		};
 		//newPage.viewportSize = { width: 1920, height: 1080 };
+		//当百度云页面打开并渲染完成时触发
 		newPage.onLoadFinished = function(status) {
 			//console.log('A child page is Loaded: ' + newPage.url);
 			//newPage.render('newPage.png', {format: 'png', quality: '100'});
 			//console.log(pwdArray.length);
 			if(pwdArray.length > 0){
 				//console.log("enter");
+				//从数组中pop出密码，当无密码时pop的数据为null字符窜
 				var temp = {"url": newPage.url.toString(), "password": pwdArray.pop().toString()};
 				console.log(JSON.stringify(temp));
-				result.push(temp);
+				result.push(temp); // 将json数据push进返回结果
 			}
 		};
 	};
@@ -64,25 +71,26 @@ server.listen(8089, function (request, response) {
 					var pwdArray = new Array();
 					var btnList = document.getElementsByClassName(dom); // 百度云链接
 					for(var i = 0; i < btnList.length;i ++ ){
-						//猜测所有节点都有密码
+						//猜测所有下载节点都有密码
 						var temp = document.getElementById("downloadPwd-" + i);
 						if(temp != undefined){
 							//console.log("****" + temp.value);
-							pwdArray.push(temp.value);
+							pwdArray.push(temp.value); // 有密码push进数组
 						}else{
 							//console.log("****null");
-							pwdArray.push("null");
+							pwdArray.push("null"); // 无密码则push进null字符窜，这样做到和url的一一对应
 						}
 					}
 					for(var i = 0; i < btnList.length;i ++ ){
 						//console.log("click");
-						btnList[i].click();
+						btnList[i].click(); // 点击下载，打开新标签页
 					}
 					return pwdArray;
 				}, dom);
 			}, 6000);
         }
     });
+	//设置等待20秒后才发送客户端的响应结果，这样保证上述方法都能成功运行结束
 	window.setTimeout(function(){
 		var rs = JSON.stringify(result)
 		console.log(rs);
